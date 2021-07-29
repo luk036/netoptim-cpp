9248be8a8596b695c9da5a25d51af40c52cea1f8
Wai-Shing Luk <luk036@gmail.com>
2021-07-29 13:50:45 +0800

    add xtensor config

--- specific.cmake
+++ specific.cmake
@@ -47,6 +47,18 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   add_compile_options(/std:c++latest /await)
 endif()
 
+CPMAddPackage("gh:xtensor-stack/xtl#0.6.23")
+if(xtl_ADDED)
+  message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
+  include_directories(${xtl_SOURCE_DIR}/include)
+endif(xtl_ADDED)
+
+CPMAddPackage("gh:xtensor-stack/xtensor#0.22.0")
+if(xtensor_ADDED)
+  message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
+  include_directories(${xtensor_SOURCE_DIR}/include)
+endif(xtensor_ADDED)
+
 CPMAddPackage(
   NAME EllAlgo
   GIT_TAG 1.0.3
