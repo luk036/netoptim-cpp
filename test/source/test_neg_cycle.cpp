c0ffb798cdb33ebf2978123d1776e5cadf35b204
Wai-Shing Luk <luk036@gmail.com>
2023-04-21 21:28:48 +0800

    try window build (2)

--- test/source/test_neg_cycle.cpp
+++ test/source/test_neg_cycle.cpp
@@ -12,37 +12,37 @@
 #include <xnetwork/classes/digraphs.hpp>     // for DiGraphS
 #include <xnetwork/generators/testcases.hpp> // for create_test_case1, crea...
 
-// /*!
-//  * @brief
-//  *
-//  * @tparam Graph
-//  * @param gra
-//  * @return true
-//  * @return false
-//  */
-// template <typename Graph> auto do_case(const Graph &gra) -> bool {
-//   const auto get_weight = [&](const auto &edge) -> int {
-//     const auto [u, v] = gra.end_points(edge);
-//     return gra[u][v];
-//   };
-//
-//   auto dist = std::vector<int>(gra.number_of_nodes(), 0);
-//   auto N = NegCycleFinder<Graph>(gra);
-//   const auto cycle = N.find_neg_cycle(dist, get_weight);
-//   return !cycle.empty();
-// }
-//
-// /*!
-//  * @brief
-//  *
-//  */
-// TEST_CASE("Test Negative Cycle") {
-//   auto weights = std::array<int, 5>{-5, 1, 1, 1, 1};
-//   auto gra = create_test_case1(weights);
-//   const auto hasNeg = do_case(gra);
-//   CHECK(hasNeg);
-// }
-//
+/*!
+ * @brief
+ *
+ * @tparam Graph
+ * @param gra
+ * @return true
+ * @return false
+ */
+template <typename Graph> auto do_case(const Graph &gra) -> bool {
+  const auto get_weight = [&](const auto &edge) -> int {
+    const auto [u, v] = gra.end_points(edge);
+    return gra[u][v];
+  };
+
+  auto dist = std::vector<int>(gra.number_of_nodes(), 0);
+  auto N = NegCycleFinder<Graph>(gra);
+  const auto cycle = N.find_neg_cycle(dist, get_weight);
+  return !cycle.empty();
+}
+
+/*!
+ * @brief
+ *
+ */
+TEST_CASE("Test Negative Cycle") {
+  auto weights = std::array<int, 5>{-5, 1, 1, 1, 1};
+  auto gra = create_test_case1(weights);
+  // const auto hasNeg = do_case(gra);
+  // CHECK(hasNeg);
+}
+
 // /*!
 //  * @brief
 //  *
