67350d1ead139ebc3ba17a260110ec63504cc5b0
Wai-Shing Luk <luk036@gmail.com>
2023-04-21 13:45:18 +0800

    use XNetwork 1.6.3; completely remove cppcoro; try windows build

--- specific.cmake
+++ specific.cmake
@@ -17,28 +17,16 @@ if(Boost_FOUND)
   target_compile_definitions(Boost::boost INTERFACE BOOST_ALL_NO_LIB=1)
 endif()
 
-# CPMAddPackage(
-#   NAME cppcoro
-#   GIT_TAG 2.0
-#   GITHUB_REPOSITORY luk036/cppcoro
-# )
-# # print_target_properties(cppcoro)
-# if(cppcoro_ADDED)
-#   message(STATUS "Found cppcoro: ${cppcoro_SOURCE_DIR}")
-#   add_library(cppcoro::cppcoro INTERFACE IMPORTED GLOBAL)
-#   target_include_directories(cppcoro::cppcoro SYSTEM INTERFACE ${cppcoro_SOURCE_DIR}/include)
+# CPMAddPackage( NAME cppcoro GIT_TAG 2.0 GITHUB_REPOSITORY luk036/cppcoro ) #
+# print_target_properties(cppcoro) if(cppcoro_ADDED) message(STATUS "Found cppcoro:
+# ${cppcoro_SOURCE_DIR}") add_library(cppcoro::cppcoro INTERFACE IMPORTED GLOBAL)
+# target_include_directories(cppcoro::cppcoro SYSTEM INTERFACE ${cppcoro_SOURCE_DIR}/include)
 # endif(cppcoro_ADDED)
 #
-# if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-#   # using GCC
-#   add_compile_options(-fcoroutines)
-# elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-#   # using clang
-#   add_compile_options(-fcoroutines-ts -stdlib=libc++)
-# elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-#   # using Visual Studio C++
-#   add_compile_options(/std:c++latest /await)
-# endif()
+# if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU") # using GCC add_compile_options(-fcoroutines)
+# elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang") # using clang add_compile_options(-fcoroutines-ts
+# -stdlib=libc++) elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") # using Visual Studio C++
+# add_compile_options(/std:c++latest /await) endif()
 
 CPMAddPackage(
   NAME Py2Cpp
@@ -49,7 +37,7 @@ CPMAddPackage(
 
 CPMAddPackage(
   NAME XNetwork
-  GIT_TAG 1.6.2
+  GIT_TAG 1.6.3
   GITHUB_REPOSITORY luk036/xnetwork-cpp
   OPTIONS "INSTALL_ONLY ON" # create an installable target
 )
@@ -74,11 +62,7 @@ CPMAddPackage(
 )
 
 set(SPECIFIC_LIBS
-    EllAlgo::EllAlgo
-    XNetwork::XNetwork
-    Py2Cpp::Py2Cpp
-    Boost::boost
+    EllAlgo::EllAlgo XNetwork::XNetwork Py2Cpp::Py2Cpp Boost::boost
     # cppcoro::cppcoro
-    Threads::Threads
-    fmt::fmt
+    Threads::Threads fmt::fmt
 )
