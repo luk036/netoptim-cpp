7a338e47b0ee83fa63e3bcbf89e37a72155d4ab2
Wai-Shing Luk <luk036@gmail.com>
2025-03-28 06:01:35 +0000

    remove greeter

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,4 +1,3 @@
-#include <netoptim/greeter.h>  // for LanguageCode, LanguageCode::DE, Langua...
 #include <netoptim/version.h>  // for NETOPTIM_VERSION
 
 #include <cxxopts.hpp>    // for value, OptionAdder, Options, OptionValue
@@ -8,13 +7,6 @@
 #include <unordered_map>  // for operator==, unordered_map, __hash_map_...
 
 auto main(int argc, char **argv) -> int {
-    const std::unordered_map<std::string, netoptim::LanguageCode> languages{
-        {"en", netoptim::LanguageCode::EN},
-        {"de", netoptim::LanguageCode::DE},
-        {"es", netoptim::LanguageCode::ES},
-        {"fr", netoptim::LanguageCode::FR},
-    };
-
     cxxopts::Options options(*argv, "A program to welcome the world!");
 
     std::string language;
@@ -40,15 +32,6 @@ auto main(int argc, char **argv) -> int {
         std::cout << "NetOptim, version " << NETOPTIM_VERSION << std::endl;
         return 0;
     }
-
-    auto langIt = languages.find(language);
-    if (langIt == languages.end()) {
-        std::cerr << "unknown language code: " << language << std::endl;
-        return 1;
-    }
-
-    // netoptim::NetOptim netoptim(name);
-    // std::cout << netoptim.greet(langIt->second) << std::endl;
 
     return 0;
 }
